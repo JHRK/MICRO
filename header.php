bc4c0d7c213ce5212225028ca976902bc2db7657
JHRK <jhrk05@gmail.com>
2023-10-25 17:34:10 -0300

    Barebones

---
+++ header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@600&display=swap" rel="stylesheet">
+    <title>Grande</title>
+    <link rel="icon" type="image/x-icon" href="./img/Logo.png">
+  </head>
+    <body class="container-fluid">
+      
+      <header class="row">
+          <h1><a href="index.html" style="margin-left: 8%; font-family:Work Sans; text-decoration:none; color:#ffffff; background-color: #2041fe; ">••MICRO••</a></h1>
+          <div class="col-12">
+                <nav>
+                  <form><input id="pesquisa-barra" type="search" placeholder="Células, Fungos..." size="162" style="margin-top: 2%;"></form></nav>
+              </div>
+            </nav>
+          </div>
+      </header>
+  
+      <div class="row">
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+include_once './header.php'
+?>
+      <main class="container-fluid col-8">
+              
+              <section class="col-lg-12 mt-3">
+                <div class="row" >
+                  <div class="col-5" style="border-right:5px solid #2041fe"><h2>Bactérias<img src=".\arrow_right_alt_FILL0_wght400_GRAD0_opsz24.png"></h2>
+                  <p>Imagens de diferentes espécies e estruturas observadas em microscópios.</p></div>
+                </div>
+              </section>
+              <section class="col-lg-12 mt-3">
+                <div class="row" >
+                  <div class="col-5" style="border-right:5px solid #2041fe"><h2>Células<img src=".\arrow_right_alt_FILL0_wght400_GRAD0_opsz24.png"></h2>
+                  <p>Imagens de diferentes espécies e estruturas observadas em microscópios.</p></div>
+                </div>
+              </section>
+              <section class="col-lg-12 mt-3">
+                <div class="row" >
+                  <div class="col-5" style="border-right:5px solid #2041fe"><h2>Tecidos<img src=".\arrow_right_alt_FILL0_wght400_GRAD0_opsz24.png"></h2>
+                  <p>Imagens de diferentes espécies e estruturas observadas em microscópios.</p></div>
+                </div>
+              </section>
+              <section class="col-lg-12 mt-3">
+                <div class="row" >
+                  <div class="col-5" style="border-right:5px solid #2041fe"><h2>Bactérias<img src=".\arrow_right_alt_FILL0_wght400_GRAD0_opsz24.png"></h2>
+                  <p>Imagens de diferentes espécies e estruturas observadas em microscópios.</p></div>
+                </div>
+              </section>
+
+      </main>   
+      <aside class="col-4">
+        <form action="./select.php">
+          <input type="radio" id="html" name="fav_language" value="oplo">
+        </form>
+      </aside>
+</div>
+  
+      <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+      <script src="script.js"></script>
+    </body>
+</html>
