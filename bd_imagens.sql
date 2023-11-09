9c0a09d84f439f9ab6ccecdfb040516990495aca
JHRK <jhrk05@gmail.com>
2023-11-09 14:53:14 -0300

    opolop

--- bd_imagens.sql
+++ bd_imagens.sql
@@ -1,60 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 01-Nov-2023 às 21:37
--- Versão do servidor: 10.4.17-MariaDB
--- versão do PHP: 7.3.27
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+
+CREATE TABLE `BD_IMAGENS` (
+  `ID` int(11) NOT NULL,
+  `SRC` varchar(45) NOT NULL,
+  `Nome` varchar(45) NOT NULL,
+  `TAG` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+INSERT INTO `BD_IMAGENS` (`ID`, `SRC`, `Nome`, `TAG`) VALUES
+(3, './Img_Strg/Group 14 (1).png', 'Microscópio Óptico Esquema', 'Esquema');
+
+
+
+ALTER TABLE `BD_IMAGENS`
+  ADD PRIMARY KEY (`ID`);
+
 
 --
--- Banco de dados: 'bd_projeto'
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela 'bd_imagens'
---
-
-CREATE TABLE 'bd_imagens' (
-  'ID' int(11) NOT NULL,
-  'Descrição' varchar(45) NOT NULL,
-  'TAG' varchar(45) NOT NULL,
-  'SRC' varchar(100) NOT NULL
-); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela 'bd_imagens'
---
-ALTER TABLE 'bd_imagens'
-  ADD PRIMARY KEY ('ID');
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela 'bd_imagens'
---
-ALTER TABLE 'bd_imagens'
-  MODIFY 'ID' int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `BD_IMAGENS`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- searched.php
+++ searched.php
@@ -5,11 +5,19 @@ include_once './header.php'
  <?php 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $pesquisa= $_POST['pesquisa_sub'];
+    $pesquisa = $_POST['pesquisa_sub'];
+
+    //Ele precisa buscar com query no BD, com uma variavel. O select está funfando
+
+    $busca = mysql_query("echo "SELECT SRC,Nome FROM `BD_IMAGENS` WHERE Nome LIKE ".$pesquisa.""");
+    SELECT SRC FROM `teste_sql` WHERE Nome LIKE '%Esquema%'
+
+
+
     if (empty($pesquisa)) {
       echo "Não funfou.";
     } else {
-      echo "$pesquisa";
+      ;
     }
   }
 
